6ecd9b948eedd377c94cd0e96496cf2d8fa82b34
Antoine Lamielle <gh@0x010c.fr>
2018-03-01 19:32:50 +0000

    Enhance UX of nearby generator
    * Add initial value to limit
    * Add error messages
    * Fix UI bugs
    * Add help messages

--- modules/generator/mw.recordWizard.generator.Nearby.js
+++ modules/generator/mw.recordWizard.generator.Nearby.js
@@ -13,10 +13,12 @@
         this.latitude = new OO.ui.TextInputWidget( { placeholder: mw.message( 'mwe-recwiz-nearby-latitude' ).text() } );
         this.longitude = new OO.ui.TextInputWidget( { placeholder: mw.message( 'mwe-recwiz-nearby-longitude' ).text() } );
         this.currentPositionButton = new OO.ui.ButtonWidget( { title: mw.message( 'mwe-recwiz-nearby-getcoordinates' ).text(), icon: 'tag' } );
-        this.limit = new OO.ui.NumberInputWidget( { min: 1, max: 500, value: 100, step: 10, isInteger: true } );
+        this.limit = new OO.ui.NumberInputWidget( { min: 1, max: 500, value: 100, step: 10, pageStep: 100, isInteger: true } );
+        this.wikidata = new OO.ui.RadioOptionWidget( { label: 'Wikidata' } );
+        this.source = new OO.ui.RadioSelectWidget( { items: [ this.wikidata ] } );
 
 		this.layout = new OO.ui.Widget( {
-	        classes: [ 'mwe-recwiz-nearby', 'mwe-recwiz-increment' ],
+	        classes: [ 'mwe-recwiz-nearby' ],
 	        content: [
 	            new OO.ui.FieldLayout(
 	                new OO.ui.Widget( {
@@ -24,17 +26,24 @@
 	                        new OO.ui.HorizontalLayout( { items: [ this.latitude, this.longitude, this.currentPositionButton ], } )
 		                ]
 		            } ), {
-	                    align: 'left',
-	                    label: mw.message( 'mwe-recwiz-nearby-coordinates' ).text()
+	                    align: 'top',
+	                    label: mw.message( 'mwe-recwiz-nearby-coordinates' ).text(),
+	                    help: mw.message( 'mwe-recwiz-nearby-coordinates-help' ).text(),
                     }
 		        ),
 	            new OO.ui.FieldLayout(
 	                this.limit, {
-	                    align: 'left',
-	                    label: mw.message( 'mwe-recwiz-nearby-limit' ).text()
+	                    align: 'top',
+	                    label: mw.message( 'mwe-recwiz-nearby-limit' ).text(),
+	                    help: mw.message( 'mwe-recwiz-nearby-limit-help' ).text(),
                     }
 		        ),
-		        this.fetchButton
+	            new OO.ui.FieldLayout(
+	                this.source, {
+	                    align: 'top',
+	                    label: mw.message( 'mwe-recwiz-nearby-source' ).text()
+                    }
+		        )
 	        ],
 	    } );
 
@@ -46,7 +55,8 @@
 
 	    this.latitude.setValue( this.params.latitude );
 	    this.longitude.setValue( this.params.longitude );
-	    this.limit.setValue( this.params.limit );
+	    this.limit.setValue( this.params.limit || this.limit.getValue() );
+        this.source.selectItem( this.wikidata );
 
         rw.generator.Generator.prototype.initialize.call( this );
     };
@@ -54,35 +64,51 @@
 
 
 	rw.generator.Nearby.prototype.getCurrentPosition = function() {
+	    this.lockUI();
         navigator.geolocation.getCurrentPosition(
             this.onPositionSuccess.bind( this ),
             this.onPositionError.bind( this ),
             {
-                timeout: 10000,
-                maximumAge: 0
+                timeout: 10000, //10 seconds
+                maximumAge: 3600000 //1 hour
             }
         );
 	};
 
 	rw.generator.Nearby.prototype.onPositionSuccess = function( pos ) {
+	    this.unlockUI();
         this.latitude.setValue( pos.coords.latitude );
         this.longitude.setValue( pos.coords.longitude );
         this.currentPositionButton.setFlags( { 'destructive': false } );
 	};
 
 	rw.generator.Nearby.prototype.onPositionError = function( error ) {
-	    //TODO: add an error message somewhere
-	    console.log( error );
-        this.currentPositionButton.setFlags( 'destructive' );
+	    this.unlockUI();
+	    var errorMessage = mw.message( 'mwe-recwiz-error-cantgetposition', error.message ).text();
+        this.showErrors( new OO.ui.Error( errorMessage, { recoverable: false } ) );
+	};
+
+	rw.generator.Nearby.prototype.lockUI = function() {
+        this.currentPositionButton.setDisabled( true );
+        this.latitude.setDisabled( true );
+        this.longitude.setDisabled( true );
+        this.latitude.pushPending();
+        this.longitude.pushPending();
+	};
+
+	rw.generator.Nearby.prototype.unlockUI = function() {
+        this.currentPositionButton.setDisabled( false );
+        this.latitude.setDisabled( false );
+        this.longitude.setDisabled( false );
+        this.latitude.popPending();
+        this.longitude.popPending();
 	};
 
 	rw.generator.Nearby.prototype.fetch = function() {
         var generator = this,
 	        lat = this.latitude.getValue(),
 	        lng = this.longitude.getValue(),
-	        limit = this.limit.getValue();
-	    // TODO: check quality of thoose inputs
-	    // if not, return this.deferred.reject( new OO.ui.Error( "bad value", { warning: true } ) );
+	        limit = parseInt( this.limit.getValue() );
 
 	    // save the inputs for a potential futur preload
 	    this.params.latitude = lat;
