47ddfcdfa7051ef1f5986bbc5b5a974763f8ed42
Antoine Lamielle <gh@0x010c.fr>
2018-03-20 14:12:36 +0000

    Remove ui substeps, merge their content directly in the step
    Since locutor and details are now 2 separated steps, having substeps is no longer justified.
    This will allow an easier ui<->controller communication.

--- modules/ui/mw.recordWizard.ui.Details.js
+++ modules/ui/mw.recordWizard.ui.Details.js
@@ -26,25 +26,122 @@
 
 		rw.ui.Step.prototype.load.call( this );
 
-		this.substeps = {
-		    param: new rw.ui.DetailsParam(),
-		    generator: new rw.ui.DetailsGenerator(),
-		};
+        this.languageSelector = new OO.ui.FieldLayout( new OO.ui.DropdownWidget( {
+	        menu: {
+		        items: [
+			        new OO.ui.MenuOptionWidget( { data: 'English', label: 'English' } ),
+			        new OO.ui.MenuOptionWidget( { data: 'French', label: 'French' } ),
+			        new OO.ui.MenuOptionWidget( { data: 'Alemannic', label: 'Alemannic' } )
+		        ]
+	        },
+	        //$overlay: ,
+        } ), {
+	        align: 'left',
+	        classes: [ 'mwe-recwiz-increment' ],
+	        label: mw.message( 'mwe-recwiz-param-lang' ).text()
+        } );
 
-		this.accordion = new rw.layout.AccordionLayout( {
-		    items: [
-		        this.substeps.param,
-		        this.substeps.generator
-		    ],
-		} );
+		this.generators = {};
 
-		this.$container.prepend( this.accordion.$element );
+        if ( rw.metadatas.generator === undefined ) {
+            rw.metadatas.generator = {};
+        }
+
+        this.windowManager = new OO.ui.WindowManager();
+        $( 'body' ).append( this.windowManager.$element );
+
+        this.wordList = new rw.layout.WordSelectorWidget( {
+	        placeholder: mw.message( 'mwe-recwiz-generator-addwords' ).text(),
+	        allowArbitrary: true,
+	        inputPosition: 'outline' //TODO: maybe inline ?
+        } );
+        if ( rw.metadatas.words !== undefined ) {
+            this.addWords( rw.metadatas.words );
+        }
+
+        this.generatorButtons = new OO.ui.ButtonGroupWidget();
+        for ( className in rw.generator ) {
+            this.setupGenerator( className );
+        }
+
+        this.layout = new OO.ui.Widget( {
+            content: [
+                new OO.ui.FieldLayout( this.wordList, {
+                    align: 'top',
+                    label: mw.message( 'mwe-recwiz-generator-wordlist' ).text(),
+                } ),
+                this.generatorButtons
+            ],
+            classes: [ 'mwe-recwiz-increment' ],
+        } );
+
+		this.wordList.on( 'change', this.onWordListUpdate.bind( this ) );
+		this.wordList.on( 'add', this.onWordListAdd.bind( this ) );
+
+		this.$container.prepend( this.layout.$element ).prepend( this.languageSelector.$element );
+	};
+
+	rw.ui.Details.prototype.setupGenerator = function( className ) {
+        var ui = this,
+            name = rw.generator[ className ].static.name;
+
+        if ( name === '__generic__' ) {
+        	return;
+        }
+
+        this.generators[ name ] = new rw.generator[ className ]( {
+            callback: this.addWords.bind( this ),
+        } );
+        this.windowManager.addWindows( [ this.generators[ name ] ] );
+
+        var button = new OO.ui.ButtonWidget( { label: this.generators[ name ].label, icon: 'add' } );
+        button.on( 'click', function() {
+            console.log( name );
+            ui.windowManager.openWindow( ui.generators[ name ] );
+        } );
+
+        this.generatorButtons.addItems( [ button ] );
+	};
+
+	rw.ui.Details.prototype.addWords = function( list ) {
+	    if ( list.length > 0 ) {
+	        for ( var i=0; i < list.length; i++ ) {
+	        	var word = list[ i ],
+	        		extra = {};
+	        	if ( typeof list[ i ] !== 'string' ) {
+	            	word = list[ i ].text;
+	            	delete list[ i ].text;
+	            	extra = list[ i ];
+	           	}
+	           	//TODO: Move this somehow to a controller
+	           	this.wordList.addTag( word );
+	           	rw.records[ word ].setExtra( extra );
+	        }
+	    }
+	};
+
+	rw.ui.Details.prototype.onWordListAdd = function( item, index ) {
+	    //TODO: Move this somehow to a controller
+	    word = item.getData();
+       	if ( rw.records[ word ] === undefined ) {
+       		rw.records[ word ] = new rw.Record( word, rw.metadatas );
+       	}
+	};
+
+	rw.ui.Details.prototype.onWordListUpdate = function() {
+		var nbWords = this.wordList.getItemCount();
+		//TODO: find a place to display word count
+		if ( nbWords === 0 ) {
+	    	//mw.message( 'mwe-recwiz-generator-noword' ).text();
+	  	}
+	  	else {
+	    	//mw.message( 'mwe-recwiz-generator-wordcount', nbWords ).text();
+	  	}
 	};
 
 	rw.ui.Details.prototype.collect = function() {
-	    for ( substep in this.substeps ) {
-	        this.substeps[ substep ].collect();
-	    }
+		//TODO collect language
+	    rw.metadatas.words = this.wordList.getValue();
 	};
 
 }( mediaWiki, jQuery, mediaWiki.recordWizard, OO ) );
--- modules/ui/mw.recordWizard.ui.Locutor.js
+++ modules/ui/mw.recordWizard.ui.Locutor.js
@@ -26,23 +26,68 @@
 
 		rw.ui.Step.prototype.load.call( this );
 
-		this.substeps = {
-		    locutor: new rw.ui.DetailsLocutor(),
-		};
+		var languages = [];
+		for ( code in rw.config.languages ) {
+			languages.push( new OO.ui.MenuOptionWidget( {
+				data: rw.config.languages[ code ].qid,
+				label: rw.config.languages[ code ].localname
+			} ) );
+		}
+		languages.sort( function( a, b ) { return a.getLabel() > b.getLabel(); } );
 
-		this.accordion = new rw.layout.AccordionLayout( {
-		    items: [
-		        this.substeps.locutor,
-		    ],
-		} );
+		this.genderSelector = new OO.ui.ButtonSelectWidget( {
+	        items: [
+		        new OO.ui.ButtonOptionWidget( { data: rw.config.items['genderMale'], label: mw.message( 'mwe-recwiz-gender-male' ).text() } ),
+		        new OO.ui.ButtonOptionWidget( { data: rw.config.items['genderFemale'], label: mw.message( 'mwe-recwiz-gender-female' ).text() } ),
+		        new OO.ui.ButtonOptionWidget( { data: rw.config.items['genderOther'], label: mw.message( 'mwe-recwiz-gender-other' ).text() } )
+	        ]
+        } );
 
-		this.$container.prepend( this.accordion.$element );
+        this.spokenLanguagesSelector = new OO.ui.CapsuleMultiselectWidget( {
+	        menu: { items: languages },
+	        //$overlay: ,
+            indicator: 'required',
+        } );
+
+        this.locationSelector = new OO.ui.TextInputWidget( {
+	        name: 'location',
+        } );
+
+        this.$content = $( '<div>' )
+            .append( new OO.ui.FieldLayout( this.genderSelector, {
+			    align: 'left',
+			    classes: [ 'mwe-recwiz-increment' ],
+			    label: mw.message( 'mwe-recwiz-locutor-gender' ).text(),
+		    } ).$element )
+	        .append( new OO.ui.FieldLayout( this.spokenLanguagesSelector, {
+			    align: 'left',
+			    classes: [ 'mwe-recwiz-increment' ],
+			    label: mw.message( 'mwe-recwiz-locutor-languages' ).text()
+		    } ).$element )
+	        .append( new OO.ui.FieldLayout( this.locationSelector, {
+			    align: 'left',
+			    classes: [ 'mwe-recwiz-increment' ],
+			    label: mw.message( 'mwe-recwiz-locutor-location' ).text()
+		    } ).$element );
+
+		// Populate
+		if ( rw.metadatas.locutor !== undefined ) {
+			this.genderSelector.selectItemByData( rw.metadatas.locutor.gender );
+			this.spokenLanguagesSelector.setItemsFromData( rw.metadatas.locutor.languages );
+			this.locationSelector.setValue( rw.metadatas.locutor.location );
+		}
+
+		this.$container.prepend( this.$content );
 	};
 
 	rw.ui.Locutor.prototype.collect = function() {
-	    for ( substep in this.substeps ) {
-	        this.substeps[ substep ].collect();
-	    }
+	    var genderItem = this.genderSelector.getSelectedItem();
+
+		rw.metadatas.locutor['gender'] = ( genderItem === null ? null : genderItem.getData() );
+		rw.metadatas.locutor['languages'] = this.spokenLanguagesSelector.getItemsData();
+		rw.metadatas.locutor['location'] = this.locationSelector.getValue();
+
+	    return rw.metadatas.locutor;
 	};
 
 }( mediaWiki, jQuery, mediaWiki.recordWizard, OO ) );
