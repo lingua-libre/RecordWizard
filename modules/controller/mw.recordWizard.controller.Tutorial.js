0c92939debff84bce11928217a8a8a6d3e335fa6
Antoine Lamielle <gh@0x010c.fr>
2018-05-15 21:22:23 +0200

    Implement the tutorial step
    It goes in two phases:
    1. ask the user for access to his microphone
    2. let the user test the configuration of its microphone

--- modules/controller/mw.recordWizard.controller.Tutorial.js
+++ modules/controller/mw.recordWizard.controller.Tutorial.js
@@ -25,25 +25,51 @@
 	rw.controller.Tutorial.prototype.load = function () {
 		rw.controller.Step.prototype.load.call( this );
 
-		// XXX do stuff
+		this.getAudioStream();
+		this.ui.on( 'reopen-audiostream-popup', this.getAudioStream.bind( this ) );
+	};
+
+	/**
+	 *
+	 */
+	rw.controller.Tutorial.prototype.getAudioStream = function () {
+		var controller = this;
+
+		// Cleanup previous recorder instance when we click on the button
+		// to reopen the popup
+		this.unloadRecorder();
+
+		this.recorder = new rw.libs.LinguaRecorder();
+
+		this.recorder.on( 'ready', function () {
+			controller.ui.switchMessage();
+			controller.recorder.start();
+			controller.recorder.on( 'recording', controller.ui.animateVolumeBar.bind( controller.ui ) );
+		} );
+		this.recorder.on( 'readyFail', this.ui.showError.bind( this.ui ) );
+	};
+
+	/**
+	 *
+	 */
+	rw.controller.Tutorial.prototype.unloadRecorder = function () {
+		if ( this.recorder === undefined || this.recorder === null ) {
+			return;
+		}
+
+		this.recorder.stop();
+		this.recorder.off( 'ready' );
+		this.recorder.off( 'recording' );
+		this.recorder = null;
 	};
 
 	/**
 	 * @inheritDoc
 	 */
-	rw.controller.Tutorial.prototype.moveNext = function () {
-		// XXX do stuff
+	rw.controller.Tutorial.prototype.unload = function () {
+		rw.controller.Step.prototype.unload.call( this );
 
-		rw.controller.Step.prototype.moveNext.call( this );
-	};
-
-	/**
-	 * @inheritDoc
-	 */
-	rw.controller.Tutorial.prototype.movePrevious = function () {
-		// XXX do stuff
-
-		rw.controller.Step.prototype.movePrevious.call( this );
+		this.unloadRecorder();
 	};
 
 }( mediaWiki, mediaWiki.recordWizard, jQuery, OO ) );
--- modules/ui/mw.recordWizard.ui.Tutorial.js
+++ modules/ui/mw.recordWizard.ui.Tutorial.js
@@ -32,14 +32,124 @@
 	 * @inheritDoc
 	 */
 	rw.ui.Tutorial.prototype.load = function () {
+		var $volumeBar;
+
 		rw.ui.Step.prototype.load.call( this );
 
-		this.$container.prepend(
-			$( '<div>' )
-				.attr( 'id', 'mwe-recwiz-tutorial' ).css( 'text-align', 'center' )
-				.append( $( '<img>' ).attr( 'src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/LinguaLibre_LOGO-04_%28cropped%29.png/450px-LinguaLibre_LOGO-04_%28cropped%29.png' ) )
-				.append( mw.message( 'mwe-recwiz-tutorial' ).parse() )
-		);
+		this.reopenPopupButton = new OO.ui.ButtonWidget( {
+			label: mw.msg( 'mwe-recwiz-tutorial-reopenpopup' ),
+			classes: [ 'mwe-recwiz-center' ]
+		} );
+
+		this.volumeMeter = new OO.ui.ProgressBarWidget( {
+			progress: 4
+		} );
+
+		this.$activateMessage = $( '<div>' )
+			.attr( 'id', 'mwe-recwiz-tutorial-activate' )
+			.append( this.parseMessage( mw.message( 'mwe-recwiz-tutorial-activate' ).parse() ) );
+		this.$buttons.append( this.reopenPopupButton.$element );
+
+		this.$configureMessage = $( '<div>' )
+			.attr( 'id', 'mwe-recwiz-tutorial-configure' )
+			.append( this.parseMessage( mw.message( 'mwe-recwiz-tutorial-configure' ).parse() ) )
+			.hide();
+		this.nextButton.$element.hide();
+
+		$volumeBar = this.$configureMessage.find( '#mwe-recwiz-tutorial-volumebar' );
+		if ( $volumeBar.length > 0 ) {
+			$volumeBar.append( this.volumeMeter.$element );
+		} else {
+			this.$configureMessage.append( this.volumeMeter.$element );
+		}
+
+		this.$container.prepend( this.$activateMessage );
+		this.$container.prepend( this.$configureMessage );
+
+		this.reopenPopupButton.on( 'click', this.emit.bind( this, 'reopen-audiostream-popup' ) );
+	};
+
+	/**
+	 *
+	 */
+	rw.ui.Tutorial.prototype.parseMessage = function ( wikicode ) {
+		return wikicode
+			.replace( /&lt;/g, '<' )
+			.replace( /&gt;/g, '>' )
+			.replace( /<volumebar\/ ?>/g, '<div id="mwe-recwiz-tutorial-volumebar"></div>' );
+	};
+
+	/**
+	 *
+	 */
+	rw.ui.Tutorial.prototype.switchMessage = function () {
+		this.$activateMessage.hide();
+		this.reopenPopupButton.$element.hide();
+		this.$configureMessage.show();
+		this.nextButton.$element.show();
+	};
+
+	/**
+	 *
+	 *
+	 * @private
+	 * @param  {Float32Array} samples Sound samples of the audio recorded since
+	 *                                last call
+	 */
+	rw.ui.Tutorial.prototype.animateVolumeBar = function ( samples ) {
+		var i, amplitude, progress, barClass,
+			amplitudeMax = 0;
+		for ( i = 0; i < samples.length; i++ ) {
+			amplitude = Math.abs( samples[ i ] );
+			if ( amplitude > amplitudeMax ) {
+				amplitudeMax = amplitude;
+			}
+		}
+
+		if ( amplitudeMax <= 0.05 ) {
+			barClass = 'mwe-recwiz-volumeMetter-silent';
+		} else if ( amplitudeMax <= 0.9 ) {
+			barClass = 'mwe-recwiz-volumeMetter-active';
+		} else {
+			barClass = 'mwe-recwiz-volumeMetter-saturate';
+		}
+
+		progress = amplitudeMax * 100 + 4; // +4 to avoid empty bar
+		this.volumeMeter.setProgress( progress );
+
+		this.volumeMeter.$element.find( '.oo-ui-progressBarWidget-bar' )
+			.removeClass( 'mwe-recwiz-volumeMetter-silent mwe-recwiz-volumeMetter-active mwe-recwiz-volumeMetter-saturate' )
+			.addClass( barClass );
+	};
+
+	/**
+	 * Show a alert popup when we can't access to the microphone.
+	 *
+	 * @param  {DOMException} mediaStreamError error Object
+	 */
+
+	rw.ui.Tutorial.prototype.showError = function ( mediaStreamError ) {
+		var message = mw.msg( 'mwe-recwiz-error-mediastream-unknow' ),
+			errorMessageAssociation = {
+				AbortError: 'mwe-recwiz-error-mediastream-technical',
+				NotAllowedError: 'mwe-recwiz-error-mediastream-notallowed',
+				NotFoundError: 'mwe-recwiz-error-mediastream-notfound',
+				NotReadableError: 'mwe-recwiz-error-mediastream-technical',
+				OverconstrainedError: 'mwe-recwiz-error-mediastream-notfound',
+				SecurityError: 'mwe-recwiz-error-mediastream-technical'
+			};
+
+		if ( typeof mediaStreamError.name === 'string' ) {
+			if ( errorMessageAssociation[ mediaStreamError.name ] !== undefined ) {
+				message = mw.msg( errorMessageAssociation[ mediaStreamError.name ] );
+			}
+		}
+
+		if ( typeof mediaStreamError.message === 'string' ) {
+			message = message.replace( /\$1/g, mediaStreamError.message );
+		}
+
+		OO.ui.alert( message );
 	};
 
 }( mediaWiki, jQuery, mediaWiki.recordWizard, OO ) );
