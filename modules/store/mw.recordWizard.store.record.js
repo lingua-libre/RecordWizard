3ff6c3b5782adfad0e9100e08d7fec8f3d25a892
Antoine Lamielle <gh@0x010c.fr>
2020-04-06 19:41:45 +0200

    T248983: Create a new record store It will contain the metadata about all records (locutor information, license,...), the order of the records and the records themselves.

---
+++ modules/store/mw.recordWizard.store.record.js
@@ -0,0 +1,51 @@
+'use strict';
+
+( function ( mw, rw ) {
+	/**
+	 * @class StateStore
+	 * @constructor
+	 */
+	var RecordStore = function () {
+		this.data = {
+			metadata: {
+				language: rw.store.config.savedLanguage,
+				license: rw.store.config.savedLicense,
+				locutor: {
+					gender: '',
+					languages: {},
+					location: '',
+					name: '',
+					qid: '',
+					main: false,
+					new: false,
+				},
+			},
+		};
+	};
+
+	RecordStore.prototype.setLocutor = function ( locutor ) {
+		this.data.metadata.locutor.gender = locutor.gender;
+		Vue.set( this.data.metadata.locutor, 'languages', locutor.languages );
+		this.data.metadata.locutor.location = locutor.location;
+		this.data.metadata.locutor.name = locutor.name;
+		this.data.metadata.locutor.qid = locutor.qid;
+		this.data.metadata.locutor.main = locutor.main || false;
+		this.data.metadata.locutor.new = locutor.new || false;
+	};
+/*
+	RecordStore.prototype.setLocutor = function ( locutor ) {
+		locutor.gender = locutor.gender || '';
+		locutor.languages = locutor.languages || {};
+		locutor.location = locutor.location || '';
+		locutor.name = locutor.name || '';
+		locutor.qid = locutor.qid || '';
+		locutor.main = locutor.main || false;
+		locutor.new = locutor.new || locutor.qid === null;
+
+		Vue.set( this.data.metadata, 'locutor', locutor);
+	};*/
+
+
+	rw.store.record = new RecordStore();
+
+}( mediaWiki, mediaWiki.recordWizard ) );
