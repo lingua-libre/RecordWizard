ee86f4ea03d8978891fc71c2d55e0de28ed322da
0x010C <gh@0x010c.fr>
2023-04-14 08:52:00 +0200

    Bugfix T312554: Repair bogous webm files with ffmpeg
    The javascript MediaRecorder produce WebM files with a lot of missing metadatas, missing duration, missing cues,...
    FFmpeg is used here to fix thoses files during upload, before they reach the stash.
    
    Note: Fixing those files on the client-side was considered, but all the javascript EBML libraries tested where ineficient to solve the issue.

--- Hooks.php
+++ Hooks.php
@@ -38,4 +38,20 @@ class RecordWizardHooks {
 		return true;
 	}
 
+	public static function onUploadStashFile( $uploadBase, $user, $props, &$error ) {
+		global $wgFFmpegLocation;
+		$tempPath = $uploadBase->getTempPath();
+
+		// MediaRecorder produce WebM files with a lot of missing metadatas (see T312554)
+		// FFmpeg is used here to fix thoses files during upload, before they reach the stash
+		if(isset($props["mime"]) && $props["mime"] == "video/webm") {
+			$metadata = unserialize($props["metadata"]);
+			if(isset($metadata["error"])) { // if the getID3 library detected an error
+				shell_exec("{$wgFFmpegLocation} -i {$tempPath} -c copy {$tempPath}-ffmpeg.webm");
+				rename("{$tempPath}-ffmpeg.webm", "{$tempPath}");
+				wfDebugLog( 'recordwizard', "Webm file '{$uploadBase->getTitle()}' from user '{$user->getName()}' fixed during stashing using ffmpeg." );
+			}
+		}
+	}
+
 }
