0464fc968a42a0de81287991b18ffa1ee719e0cd
0x010C <gh@0x010c.fr>
2023-08-14 00:55:35 +0200

    Update the lingua-recorder library

--- bin/build.sh
+++ bin/build.sh
@@ -3,9 +3,7 @@
 mkdir -p lib/lingua-recorder ;
 cp -v node_modules/lingua-recorder/src/*.js lib/lingua-recorder/ ;
 
-for f in lib/lingua-recorder/*.js ; do
-    filename=$(basename "$f") ;
-    echo "mediaWiki.recordWizard.libs.${filename%.*} = ${filename%.*};" >> $f ;
-done
+echo "mediaWiki.recordWizard.libs.AudioRecord = AudioRecord;" >> lib/lingua-recorder/AudioRecord.js ;
+echo "mediaWiki.recordWizard.libs.LinguaRecorder = LinguaRecorder;" >> lib/lingua-recorder/LinguaRecorder.js ;
 
 cp -v node_modules/vue/dist/vue.min.js lib/ ;
